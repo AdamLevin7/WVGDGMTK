39de286d5d36c9d1bd7598bae62ba3c333f6be17
Reggie <bs19293949@outlook.com>
2022-07-16 14:40:11 -0700

    Added dice animation
    Made the dice roll and pause every 2 seconds

--- GMTK 2022.2/Assets/Scripts/DiceRollScript.cs
+++ GMTK 2022.2/Assets/Scripts/DiceRollScript.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dicerollscript : MonoBehaviour
+public class DiceRollScript : MonoBehaviour
 {
     private int diceroll;
     private int[] traps = new int[6];
     private int[] trapnumber = { 1, 2, 3, 4, 5, 6 };
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(InfiniteLoop());
+        /*
         int counter = 0;
         while (counter < 5)
         {
@@ -23,12 +26,34 @@ public class Dicerollscript : MonoBehaviour
             }
         }
         Debug.Log(traps);
+        */
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    private IEnumerator InfiniteLoop()
+    {
+        WaitForSeconds waitTime = new WaitForSeconds(2);
+        while (true)
+        {
+            //var aa;
+            Roll();
+            if (animator.GetBool("isRolling") == true)
+            {
+                animator.SetBool("isRolling", false);
+                animator.SetInteger("roll", diceroll);
+            }
+            else
+            {
+                animator.SetBool("isRolling", true);
+            }
+            yield return waitTime;
+            
+        }
     }
 
     void Roll()
