6413f63c60862dc0a18e0477124ce7aaa80ad5f3
Reggie <bs19293949@outlook.com>
2022-07-15 23:25:58 -0700

    Player Animation
    Made player idle and run animation also added shelf sprite

--- GMTK 2022.2/Assets/Scripts/PlayerMovement.cs
+++ GMTK 2022.2/Assets/Scripts/PlayerMovement.cs
@@ -6,12 +6,16 @@ public class PlayerMovement : MonoBehaviour
 {
     public float MoveSpeed;
     public Rigidbody2D rb;
+    public Animator animator;
 
     private Vector2 MoveDirection;
+    private bool facingRight;
 
     void Update()
     {
         ProcessInputs();
+
+        animator.SetFloat("Speed", Mathf.Abs((MoveDirection.x) * MoveSpeed) + Mathf.Abs((MoveDirection.y) * MoveSpeed));
     }
 
     void FixedUpdate()
@@ -25,10 +29,26 @@ public class PlayerMovement : MonoBehaviour
         float MoveY = Input.GetAxisRaw("Vertical");
 
         MoveDirection = new Vector2(MoveX, MoveY).normalized;
+
+        if(facingRight == true && MoveX > 0)
+        {
+            flip();
+        } else if(facingRight == false && MoveX < 0)
+        {
+            flip();
+        }
     }
 
     void Move()
     {
         rb.velocity = new Vector2(MoveDirection.x * MoveSpeed, MoveDirection.y * MoveSpeed);
+    }
+
+    void flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
     }
 }
