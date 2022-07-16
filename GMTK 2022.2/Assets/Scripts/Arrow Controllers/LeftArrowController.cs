be96828c36ba08995e3f2a2618f91558c669a094
Reggie <bs19293949@outlook.com>
2022-07-16 09:58:00 -0700

    Added all arrow directions
    title

---
+++ GMTK 2022.2/Assets/Scripts/Arrow Controllers/LeftArrowController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftArrowController : MonoBehaviour
+{
+    public GameObject block;
+
+    void Start()
+    {
+        ShootLeft();
+    }
+
+    void ShootLeft()
+    {
+        for (int y = -8; y < 9; y += 2)
+        {
+            Quaternion rot = Quaternion.Euler(0, 0, 180);
+            Instantiate(block, new Vector3(12, y, 0), rot);
+        }
+    }
+}
