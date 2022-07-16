a357fc35edead61e5ddaa7cce9735d6a13aca35d
Reggie <bs19293949@outlook.com>
2022-07-16 08:28:19 -0700

    Down Arrow Thing
    Carrots now shoot in a line from the top of the screen.

---
+++ GMTK 2022.2/Assets/Scripts/VerticalArrowController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalArrowController : MonoBehaviour
+{
+    public GameObject block;
+
+    private void Start()
+    {
+        ShootDown();
+    }
+
+    void ShootDown()
+    {
+        for(int x = -8; x < 9; x += 2)
+        {
+            Quaternion rot = Quaternion.Euler(0, 0, -90);
+            Instantiate(block, new Vector3(x, 12, 0), rot);
+        }
+    }
+}
